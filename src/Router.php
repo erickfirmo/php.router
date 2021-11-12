e514f69da1e3476c458ba847151a68337e73569c
Érick Firmo <erickfirmo1996@gmail.com>
2021-11-12 19:04:56 -0300

    Fixing object router visibility

--- src/Router.php
+++ src/Router.php
@@ -4,40 +4,46 @@ namespace ErickFirmo;
 
 class Router
 {
-    public $namespace = '';
-    public $routeName;
-    public $getRoutes = [];
-    public $postRoutes = [];
-    public $putRoutes = [];
-    public $patchRoutes = [];
-    public $deleteRoutes = [];
-    public $routeList = [];
-    public $method;
-    public $controller;
-    public $parameterIndex = null;
-    public $parameterValue = null;
-    public $notFoundView = null;
-    public $arguments = [];
-    public $acceptedHttpMethods = ['get', 'post', 'put', 'patch', 'delete'];
-    public $httpMethod = 'get';
-    public $route = [];
-    public $request;
-    public $requestVarName = 'request';
+    private $namespace = '';
+    private $routeName;
 
-    // Define namespace dos controllers
-    public function setNamespace(string $namespace) : void
+    private $routeList = [];
+    private $method;
+    private $controller;
+    private $notFoundView = null;
+    private $arguments = [];
+    private $acceptedHttpMethods = ['get', 'post', 'put', 'patch', 'delete'];
+    private $httpMethod = 'get';
+    private $route = [];
+    private $request;
+    private $requestVarName = 'request';
+
+    // Retorna método de requisição http
+    private function request_method() : string
     {
-        $this->namespace = $namespace;
+        return $_SERVER['REQUEST_METHOD'];
+    }
+    
+    // Retorna path da url atual
+    private function request_path() : string
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
-    // Retorna namespace dos controllers
-    public function getNamespace() : string
+    // Verifica e define verbo http
+    private function checkHttpMethod() : bool
     {
-        return $this->namespace;
+        if($this->request_method() == 'POST')
+            $this->httpMethod = (isset($_POST['_method']) && in_array($_POST['_method'], $this->acceptedHttpMethods)) ? $_POST['_method'] : 'post';
+
+        if(!$this->route)
+            return false;
+
+        return $this->route['http_method'] == $this->httpMethod ? true : false;
     }
 
     // Cria a rota
-    public function createRoute(string $httpMethod, string $path, string $controller, string $method, string $name='') : array
+    private function createRoute(string $httpMethod, string $path, string $controller, string $method, string $name='') : array
     {
         // Criando rota
         $name = !$name ? $path : $name;
@@ -51,7 +57,7 @@ class Router
     }
 
     // Cria mapa de chaves dos parametros passados na rota
-    public function createSegmentsMap(string $path, array $segments_map=[]) : array
+    private function createSegmentsMap(string $path, array $segments_map=[]) : array
     {
         $array_path = explode('/', $path);
         array_shift($array_path);
@@ -62,22 +68,159 @@ class Router
         return $segments_map;
     }
 
-    // Retorna método de requisição http
-    public function request_method() : string
+    // Define nome da rota e argumentos que serão passados
+    private function setRoute(array $route, array $segments_map) : void
     {
-        return $_SERVER['REQUEST_METHOD'];
+        $array_url = explode('/', $this->request_path());
+        array_shift($array_url);
+
+        $args = [];
+        
+        foreach ($segments_map as $key => $segment) {
+            if(array_key_exists($key, $array_url)) {
+                array_push($args, $array_url[$key]);
+                $array_url[$key] = $segment;
+            }
+        }
+
+        $routeName = '/' . implode('/', $array_url);
+
+        $route['segments'] = $args;
+        
+        if($routeName == $route['path'] )
+        {
+            if(!isset($this->routeList[$this->httpMethod]))
+            {
+                $this->route = $route;
+            } elseif(!array_key_exists($this->request_path(), $this->routeList[$this->httpMethod])) {
+                $this->route = $route;
+            }
+        }
+
+        // Inserindo na lista de rotas
+        $this->routeList[$route['http_method']][$route['path']] = $route;
     }
-    
-    // Retorna path da url atual
-    public function request_path() : string
+
+
+    // Retorna namespace dos controllers
+    private function getNamespace() : string
     {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        return $this->namespace;
     }
-    
+
+
+    // Define namespace dos controllers
+    public function setNamespace(string $namespace) : void
+    {
+        $this->namespace = $namespace;
+    }
+
     // Define arquivo de visualização para erro 404
     public function notFoundView(string $view) : void
     {
         $this->notFoundView = $view;
+    }
+
+    // Define request
+    public function setRequest(string $requestVarName, $request) : void
+    {
+        if (!is_array($request) && !is_object($request)) {
+            throw new \InvalidArgumentException('Argument 2 passed to '.__METHOD__.' must be of the type Object or array, '.gettype($request).' given.');
+        }
+
+        $this->requestVarName = $requestVarName;
+        $this->request = $request;
+    }
+
+    // Pega array de parametros de um método
+    private function get_method_argNames(string $class, string $method) : array
+    {
+        $reflectionMethod =  new \ReflectionMethod($class, $method);
+
+        $params = $reflectionMethod->getParameters();
+
+        $paramNames = array_map(function( $item ){
+            return $item->getName();
+        }, $params);
+
+        return $paramNames;
+    }
+
+    // Executa rota já definida
+    public function run()
+    {
+        try {
+            // Define verbo http da requisição atual
+            if(!$this->checkHttpMethod()) {
+                // exception
+                http_response_code(404);
+                if(!$this->notFoundView) {
+                    header("HTTP/1.0 404 Not Found");
+                    print '404 Not Found';
+                    exit();
+                }
+                include $this->notFoundView;
+                exit();
+            }
+
+            // Pega e inverte array de argumentos da rota
+            $this->arguments = $this->route['segments'];
+
+            // Definindo controller e método
+            $controllerName = $this->route['controller'];
+            $methodName = $this->route['method'];
+
+            // Invoca array de parametros do método que será chamado
+            $ReflectionMethod =  new \ReflectionMethod($controllerName, $methodName);
+            $params = $ReflectionMethod->getParameters();
+            $paramNames = array_map(function( $item ){
+                return $item->getName();
+            }, $params);
+
+            // Verifica se existe $request como argmento do método
+            if(isset($paramNames[0]) && $paramNames[0] == $this->requestVarName && $this->request)
+            {
+                // Adiciona objeto $request a lista de argumentos
+                $this->arguments = array_reverse($this->arguments);
+                array_push($this->arguments, $this->request);
+                $this->arguments = array_reverse($this->arguments);
+            }
+
+            // Validando tipo de dado dos parametros
+            foreach ($params as $key => $p)
+            {
+                // Verifica se há tipo configurado
+                if($p->getType()) {
+                    // Verifica se é um inteiro
+                    if(isset($this->arguments[$key]))
+                    {
+                        if($p->getType()->getName() == 'int')
+                        {
+                            if (!is_numeric($this->arguments[$key])) {
+                                throw new \InvalidArgumentException('Argument '.($key+1).' passed to Router must be of the type '.$p->getType()->getName().', '.gettype($this->arguments[$key]).' given.');
+                            }
+        
+                        // Verifica se é uma string
+                        } elseif($p->getType()->getName() == 'string') {
+                            if (!is_string($this->arguments[$key])) {
+                                throw new \InvalidArgumentException('Argument '.($key+1).'2 passed to Router must be of the type '.$p->getType()->getName().', '.gettype($this->arguments[$key]).' given.');
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        
+            return call_user_func_array([new $controllerName(), $methodName], $this->arguments);
+            
+        } catch (\Throwable $th) {
+
+            throw $th;
+
+        } catch (\Exception $e) {
+            
+            throw $e;
+        }
     }
 
     // Cria rota GET
@@ -159,152 +302,5 @@ class Router
 
         // Define nome da rota que será executada
         $this->setRoute($route, $segments_map);
-    }
-
-    // Verifica e define verbo http
-    public function checkHttpMethod() : bool
-    {
-        if($this->request_method() == 'POST')
-            $this->httpMethod = (isset($_POST['_method']) && in_array($_POST['_method'], $this->acceptedHttpMethods)) ? $_POST['_method'] : 'post';
-
-        if(!$this->route)
-            return false;
-
-        return $this->route['http_method'] == $this->httpMethod ? true : false;
-    }
-
-    // Define request
-    public function setRequest(string $requestVarName, $request) : void
-    {
-        if (!is_array($request) && !is_object($request)) {
-            throw new \InvalidArgumentException('Argument 2 passed to '.__METHOD__.' must be of the type Object or array, '.gettype($request).' given.');
-        }
-
-        $this->requestVarName = $requestVarName;
-        $this->request = $request;
-    }
-
-    // Define nome da rota e argumentos que serão passados
-    public function setRoute(array $route, array $segments_map) : void
-    {
-        $array_url = explode('/', $this->request_path());
-        array_shift($array_url);
-
-        $args = [];
-        
-        foreach ($segments_map as $key => $segment) {
-            if(array_key_exists($key, $array_url)) {
-                array_push($args, $array_url[$key]);
-                $array_url[$key] = $segment;
-            }
-        }
-
-        $routeName = '/' . implode('/', $array_url);
-
-        $route['segments'] = $args;
-        
-        if($routeName == $route['path'] )
-        {
-            if(!isset($this->routeList[$this->httpMethod]))
-            {
-                $this->route = $route;
-            } elseif(!array_key_exists($this->request_path(), $this->routeList[$this->httpMethod])) {
-                $this->route = $route;
-            }
-        }
-
-        // Inserindo na lista de rotas
-        $this->routeList[$route['http_method']][$route['path']] = $route;
-    }
-
-    // Executa rota já definida
-    public function run()
-    {
-        try {
-            // Define verbo http da requisição atual
-            if(!$this->checkHttpMethod()) {
-                // exception
-                http_response_code(404);
-                if(!$this->notFoundView) {
-                    header("HTTP/1.0 404 Not Found");
-                    print '404 Not Found';
-                    exit();
-                }
-                include $this->notFoundView;
-                exit();
-            }
-
-            // Pega e inverte array de argumentos da rota
-            $this->arguments = $this->route['segments'];
-
-            // Definindo controller e método
-            $controllerName = $this->route['controller'];
-            $methodName = $this->route['method'];
-
-            // Invoca array de parametros do método que será chamado
-            $ReflectionMethod =  new \ReflectionMethod($controllerName, $methodName);
-            $params = $ReflectionMethod->getParameters();
-            $paramNames = array_map(function( $item ){
-                return $item->getName();
-            }, $params);
-
-            // Verifica se existe $request como argmento do método
-            if(isset($paramNames[0]) && $paramNames[0] == $this->requestVarName && $this->request)
-            {
-                // Adiciona objeto $request a lista de argumentos
-                $this->arguments = array_reverse($this->arguments);
-                array_push($this->arguments, $this->request);
-                $this->arguments = array_reverse($this->arguments);
-            }
-
-            // Validando tipo de dado dos parametros
-            foreach ($params as $key => $p)
-            {
-                // Verifica se há tipo configurado
-                if($p->getType()) {
-                    // Verifica se é um inteiro
-                    if(isset($this->arguments[$key]))
-                    {
-                        if($p->getType()->getName() == 'int')
-                        {
-                            if (!is_numeric($this->arguments[$key])) {
-                                throw new \InvalidArgumentException('Argument '.($key+1).' passed to Router must be of the type '.$p->getType()->getName().', '.gettype($this->arguments[$key]).' given.');
-                            }
-        
-                        // Verifica se é uma string
-                        } elseif($p->getType()->getName() == 'string') {
-                            if (!is_string($this->arguments[$key])) {
-                                throw new \InvalidArgumentException('Argument '.($key+1).'2 passed to Router must be of the type '.$p->getType()->getName().', '.gettype($this->arguments[$key]).' given.');
-                            }
-                        }
-                    }
-                    
-                }
-            }
-        
-            return call_user_func_array([new $controllerName(), $methodName], $this->arguments);
-            
-        } catch (\Throwable $th) {
-
-            throw $th;
-
-        } catch (\Exception $e) {
-            
-            throw $e;
-        }
-    }
-
-    // Pega array de parametros de um método
-    protected function get_method_argNames(string $class, string $method) : array
-    {
-        $reflectionMethod =  new \ReflectionMethod($class, $method);
-
-        $params = $reflectionMethod->getParameters();
-
-        $paramNames = array_map(function( $item ){
-            return $item->getName();
-        }, $params);
-
-        return $paramNames;
     }
 }
